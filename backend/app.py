80a3105b5cd4176f180758f5401ae95c8b80e552
nachopinar <136358447+nachopinar@users.noreply.github.com>
2025-07-06 18:42:54 -0300

    cambios en app.py

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,8 @@ from routes.mesa import mesa_bp
 from routes.empleados import login_presidente_bp
 from routes.eleccion import eleccion_bp
 from routes.admin.loginAdmin import login_admin_bp
-<<<<<<< HEAD
-from routes.admin.admin import admin_bp
-from routes.votantes import votantes_bp
-from routes.votos_circuito import votos_bp
-=======
 from routes.admin.EntidadesPrincipales import admin_bp
 from routes.votanteCircuito import votacion_bp  # 👈 nuevo import
->>>>>>> 99207b835a4adb13d2c3d661d9b49dd02f3d52c3
 
 app = Flask(__name__)
 CORS(app)
@@ -26,23 +20,9 @@ app.register_blueprint(login_presidente_bp, url_prefix="/api/login_presidente")
 app.register_blueprint(eleccion_bp, url_prefix="/api/eleccion")
 app.register_blueprint(login_admin_bp, url_prefix="/api/login_admin")
 app.register_blueprint(admin_bp, url_prefix="/api/admin")
-<<<<<<< HEAD
-app.register_blueprint(votantes_bp, url_prefix="/api/votantes")
-
-@app.route('/debug/routes')
-def list_routes():
-    import urllib
-    output = []
-    for rule in app.url_map.iter_rules():
-        methods = ','.join(rule.methods)
-        line = urllib.parse.unquote(f"{rule.endpoint}: {rule.rule} [{methods}]")
-        output.append(line)
-    return '<br>'.join(sorted(output))
-=======
 app.register_blueprint(votacion_bp, url_prefix="/api/votacion")
 
 
->>>>>>> 99207b835a4adb13d2c3d661d9b49dd02f3d52c3
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
